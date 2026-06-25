2e7ee271d36f628d91a7287578a70162f80561ce
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-06-25 13:21:12 +0500

    updated graph code

--- utils/graphs.py
+++ utils/graphs.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 
 def plot(x, y, title, x_label, y_label):
-    plt.plot(x, y, marker="o")
+    plt.scatter(x, y, marker="o")
+    plt.axhline(0, color="black", linestyle="--", linewidth=0.8)
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
