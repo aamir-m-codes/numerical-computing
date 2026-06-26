4078a8cc2a51b90450028514767b92298c7631b0
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-06-26 14:30:34 +0500

    updated newton method input section

--- Newton_Raphson.py
+++ Newton_Raphson.py
@@ -1,3 +1,6 @@
+import utils.input as inp
+
+
 def newton_raphson(f, d_f, start, n):
     x = start
     for i in range(n):
@@ -6,14 +9,32 @@ def newton_raphson(f, d_f, start, n):
     return x
 
 
-def f(x):
-    return x**2 - 4
+if __name__ == "__main__":
+    schema = [
+        {
+            "name": "diff",
+            "prompt": "Enter f(x): ",
+            "type": "diff",
+        },
+        {
+            "name": "start",
+            "prompt": "Enter X0 (default 0.0): ",
+            "type": "float",
+            "default": 0.0,
+        },
+        {
+            "name": "n",
+            "prompt": "Enter iterations (default 100): ",
+            "type": "int",
+            "default": 100,
+        },
+    ]
 
-
-def d_f(x):
-    return 2 * x
-
-
-start = 3
-n = 4
-print(newton_raphson(f, d_f, start, n))
+    inputs = inp.user_input(schema=schema)
+    result = newton_raphson(
+        f=inputs["diff"][1],
+        d_f=inputs["diff"][3],
+        start=inputs["start"],
+        n=inputs["n"],
+    )
+    print(f"Result: {result:.3f}")
