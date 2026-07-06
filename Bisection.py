64634400b3d176cb50b073742ed11603dca29836
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-07-06 08:24:19 +0500

    feature: intro and implement history matrix

--- Bisection.py
+++ Bisection.py
@@ -18,9 +18,17 @@ def bisection(f, x_l, x_r, no_iter, tol=0.1):
     if f_l * f_r > 0:
         raise ValueError(f"Same sign broken: f({x_l})={f_l}, f({x_r})={f_r} ")
 
-    for i in range(no_iter):
+    # Columns: [Iteration, lower bound, upper bound, mid, f(mid), interval width]
+    # interval width -> abs(x_r - x_l)
+    history_matrix = []
+
+    for i in range(1, no_iter + 1):
+        width = abs(x_r - x_l)
         x_m = float(x_l + x_r) / 2.0
         f_m = f(x_m)
+
+        history_matrix.append([i, x_l, x_r, x_m, f_m, width])
+
         if f_m * f_l > 0:
             x_r = x_m
             f_l = f_m
@@ -30,15 +38,13 @@ def bisection(f, x_l, x_r, no_iter, tol=0.1):
         X.append(x_m)
         Y.append(f_m)
 
-        print(f"Iteration: {i + 1}\t\tMid: {x_m}\t\tf(mid): {f_m}")
+        print(f"Iteration: {i}\t\tMid: {x_m}\t\tf(mid): {f_m}")
 
-        if abs(float(x_r - x_l)) < tol or f_m == 0.0:
-            print(
-                f"Width between intervals are {x_r} - {x_l} = {abs(float(x_r - x_l))}"
-            )
-            return X, Y
+        if width < tol or f_m == 0.0:
+            print(f"Width between intervals are {x_r} - {x_l} = {width}")
+            return X, Y, history_matrix
 
-    return X, Y
+    return X, Y, history_matrix
 
 
 if __name__ == "__main__":
@@ -66,7 +72,7 @@ if __name__ == "__main__":
         },
     ]
     inputs = inp.user_input(schema=schema)
-    X, Y = bisection(
+    X, Y, history_matrix = bisection(
         f=inputs["func"][1],
         x_l=inputs["low"],
         x_r=inputs["upper"],
